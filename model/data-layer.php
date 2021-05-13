65411f4e0f0f883c32543b3664e9acaf5ba6454d
Jake Donaldson <Jdonaldson8@mail.greenriver.edu>
2021-05-13 13:24:25 -0400

    Created basic structure & model

---
+++ model/data-layer.php
@@ -0,0 +1,20 @@
+<?php
+
+/* data-layer.php
+ *Return data for the diner app
+ *
+ */
+
+// Get the meals for the order form
+function getFlavors()
+{
+    $arr = array();
+    $arr['grasshopper'] = 'The Grasshopper';
+    $arr['maple'] = 'Whiskey Maple Bacon';
+    $arr['carrot'] = 'Carrot Walnut';
+    $arr['caramel'] = 'Salted Caramel Cupcake';
+    $arr['velvet'] = 'Red Velvet';
+    $arr['lemon'] = 'Lemon Drop';
+    $arr['tiramisu'] = 'Tiramisu';
+    return $arr;
+}
